c234e6a240064958b63fbe45f62a474a800eb4fa
toni69ulloa <57428298+toni69ulloa@users.noreply.github.com>
2025-08-20 11:07:46 -0600

    Update ver.js

--- ver.js
+++ ver.js
@@ -32,8 +32,7 @@ fetch(`pelicula-${id}.json`)
           <h2>${peli.titulo}</h2>
           <p>${peli.descripcion}</p><br>
           <p><strong>Duración:</strong> ${peli.duracion}</p><br>
-          <p><strong>Calidad:</strong> 
-          <span class="badge-calidad badge-${peli.calidad.toLowerCase()}">${peli.calidad}</span></p>
+          <p><strong>Calidad:</strong><span class="badge-calidad badge-${peli.calidad.toLowerCase()}">${peli.calidad}</span></p>
           <p><strong>Idioma:</strong> ${peli.idioma} ${bandera}</p><br>
           <p><strong>Estreno:</strong> ${peli.estreno}</p>
         </div>
@@ -56,5 +55,6 @@ function cambiarReproductor(url, boton) {
   document.querySelectorAll(".botones-reproductores button").forEach(b => b.classList.remove("activo"));
   boton.classList.add("activo");
 }
+
 
 
